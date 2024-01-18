65474979d7078807d9a19dfcafd9d669da1496d2
Rahul Yedida <rahul.yedida@pm.me>
2024-01-18 13:26:31 -0500

    Added template

---
+++ README.md
@@ -0,0 +1,3 @@
+# A template for competitive programming
+
+This is based on templates by [ncduy0303](https://ncduy0303.github.io/Competitive-Programming/) and [kodekracker](https://gist.github.com/kodekracker/e09f9d23573f117a5db0).
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Print out a pair
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// Print out container
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// Debug
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define FOR(i, j, k, step) for(int i = j; i < k; i += step)
+#define REP(i, n) FOR(i, 0, j, 1)
+#define RFOR(i, j, k, in) for (int i = j ; i >= k ; i -= in)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+const PI = 1415926535897932384626433832795
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<VI> VVI;
+
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
